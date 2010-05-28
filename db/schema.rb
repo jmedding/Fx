db42caacc7238ca2d9f9e39af8be644b57f74a89
Jon Medding <jonmedding+github@gmail.com>
2010-05-28 06:32:26 +0200

    removed rails version checking

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100519145353) do
+ActiveRecord::Schema.define(:version => 20100526100729) do
 
   create_table "currencies", :force => true do |t|
     t.string   "symbol"
